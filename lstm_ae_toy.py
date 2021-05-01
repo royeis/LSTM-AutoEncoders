5e23fb5a7fe1cd0f84e60496b69e227ccce3ca58
royeis <royeisen@gmail.com>
2021-05-01 17:19:39 +0300

    toy dataset script organization

--- lstm_ae_toy.py
+++ lstm_ae_toy.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 from Model import LSTM_AutoEncoder
 from generate_toy_dataset import create_toy_data
-from Utils import prepare_tensor_dataset
+from Utils import prepare_tensor_dataset, set_all_seeds
 
 
 def plot_signals(signals):
@@ -39,21 +39,14 @@ def plot_signals_with_rec(signals, signals_rec):
     fig.show()
 
 
-def set_all_seed(seed):
-    random.seed(seed)
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
-
-
 if __name__ == '__main__':
-    set_all_seed(2021)
+    set_all_seeds(2021)
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-    print(device)
+    print(f'device used: {device}')
+
     do_train = 0
-    do_test = 0
-    do_val = 1
+    do_val = 0
+    do_test = 1
 
     x_train, x_validate, x_test = create_toy_data()
     # plot_signals(x_train[:3])
@@ -65,8 +58,8 @@ if __name__ == '__main__':
     test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=True)
 
     model = LSTM_AutoEncoder(input_size=1,
-                             enc_hidden_size=256,
-                             dec_hidden_size=256,
+                             enc_hidden_size=512,
+                             dec_hidden_size=512,
                              enc_n_layers=1,
                              dec_n_layers=1,
                              activation=nn.Sigmoid
@@ -75,7 +68,7 @@ if __name__ == '__main__':
     criterion = nn.MSELoss(reduction='sum')
 
     if do_train:
-        t = trange(500)
+        t = trange(400)
         for epoch in t:
             model.train()
             losses = []
@@ -92,31 +85,32 @@ if __name__ == '__main__':
                 optimizer.step()
                 optimizer.zero_grad()
 
-            valid_losses = []
-            model.eval()
-            with torch.no_grad():
-                for val_batch in validate_dataloader:
-                    x = val_batch[0].to(device)
-                    x_rec = model(x)
-                    loss = criterion(x, x_rec)
-                    valid_losses.append(loss.item())
+            # validation after each training epoch
+            if do_val:
+                valid_losses = []
+                model.eval()
+                with torch.no_grad():
+                    for val_batch in validate_dataloader:
+                        x = val_batch[0].to(device)
+                        x_rec = model(x)
+                        loss = criterion(x, x_rec)
+                        valid_losses.append(loss.item())
+                if epoch % 10 == 0:
+                    plot_signals_with_rec(x[:3].detach().cpu(), x_rec[:3].detach().cpu())
 
-                t.set_description('epoch {} train_loss {:.2f} valid_loss{:.2f} '
+                t.set_description('epoch {} train loss {:.2f} valid loss{:.2f} '
                                   .format(epoch, np.mean(losses), np.mean(valid_losses)))
-            torch.save(model.state_dict(), 'toy_data_model_weights.pth')
+        torch.save(model.state_dict(), 'toy_data_model_weights.pth')
 
     if do_test:
-        torch.load('toy_data_model_weights.pth')
+        model.load_state_dict(torch.load('toy_data_model_weights.pth'))
         model.eval()
-        for batch in test_dataloader:
-            x = batch[0].to(device)
-            x_rec = model(x)
-            plot_signals_with_rec(x[:3].detach().cpu(), x_rec[:3].detach().cpu())
-
-    if do_val:
-        model.eval()
+        test_losses = []
         with torch.no_grad():
-            for val_batch in validate_dataloader:
-                x = val_batch[0].to(device)
+            for batch in test_dataloader:
+                x = batch[0].to(device)
                 x_rec = model(x)
-                plot_signals_with_rec(x[:3].detach().cpu(), x_rec[:3].detach().cpu())
+                loss = criterion(x, x_rec)
+                test_losses.append(loss.item())
+        plot_signals_with_rec(x[:3].detach().cpu(), x_rec[:3].detach().cpu())
+        print('test loss: {:.2f}'.format(np.mean(test_losses)))
