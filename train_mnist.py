4bd3a07a2d09095f952b40d307850d17ed6b5dec
ilanNaiman <ilan.naiman@gmail.com>
2021-05-31 00:21:46 +0300

    edit mnist & toy plots

--- train_mnist.py
+++ train_mnist.py
@@ -13,6 +13,7 @@ from tqdm import trange
 import matplotlib.pyplot as plt
 
 from Model import LSTM_AutoEncoder
+from Utils import plot_err_epoch
 
 
 def set_all_seed(seed):
@@ -27,23 +28,24 @@ if __name__ == '__main__':
 
     # hyperparameters
     parser = argparse.ArgumentParser(description="MNIST Task")
-    parser.add_argument("--epochs", type=int, default=150)
+    parser.add_argument("--epochs", type=int, default=100)
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--optimizer", type=str, default="Adam")
     parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--clip", type=float, default=1)
     parser.add_argument("--num_layers", type=int, default=1)
     parser.add_argument("--hidden_size", type=int, default=64)
-    parser.add_argument("--print_every", type=int, default=30)
     parser.add_argument("--seed", type=int, default=2021)
 
     args = parser.parse_args()
+    suff = 'lr={:.5f}_bs={}_hs={}_clip={:.2f}'.format(args.lr, args.batch_size, args.hidden_size,
+                                                      args.clip)
 
     set_all_seed(args.seed)
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
     print(device)
-    do_train = 0
-    do_test = 1
+    do_train = 1
+    do_test = 0
     do_val = 0
     classify = 1
 
@@ -71,26 +73,28 @@ if __name__ == '__main__':
     inp_size = 28
 
     model = LSTM_AutoEncoder(input_size=inp_size,
-                             enc_hidden_size=128,
-                             dec_hidden_size=128,
-                             enc_n_layers=1,
-                             dec_n_layers=1,
+                             enc_hidden_size=args.hidden_size,
+                             dec_hidden_size=args.hidden_size,
+                             enc_n_layers=args.num_layers,
+                             dec_n_layers=args.num_layers,
                              activation=nn.Sigmoid,
                              n_categories=10
                              ).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
     criterion = nn.MSELoss()
     criterion_classify = nn.CrossEntropyLoss()
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
 
-    # α = 1e4
-
     if do_train:
-        t = trange(50)
+        t = trange(args.epochs)
         x, x_rec = None, None
+        train_losses = []
+        train_acc = []
+        val_losses = []
+        val_acc = []
         for epoch in t:
             model.train()
-            losses = []
+            epoch_losses, epoch_acc = [], []
             for batch in train_loader:
                 x = batch[0].squeeze().to(device)
                 y = batch[1].to(device)
@@ -101,7 +105,7 @@ if __name__ == '__main__':
 
                 if classify:
                     loss += criterion_classify(c, y)
-                losses.append(loss.item())
+                epoch_losses.append(loss.item())
                 loss.backward()
                 nn.utils.clip_grad_norm_(model.parameters(), 1.0)
                 optimizer.step()
@@ -111,8 +115,12 @@ if __name__ == '__main__':
                 x_rec.detach()
                 c.detach()
 
-                t.set_description('epoch {} train_loss {:.2f} '
-                                  .format(epoch, np.mean(losses)))
+                epoch_acc.append(torch.sum(torch.argmax(F.softmax(c), dim=1) == y).item() / float(len(c)))
+
+                t.set_description('epoch {} train_loss {:.2f} train_acc {:.2f}'
+                                  .format(epoch, np.mean(epoch_losses), np.mean(epoch_acc)))
+            train_losses.append(np.mean(epoch_losses))
+            train_acc.append(np.mean(epoch_acc))
 
             model.eval()
             with torch.no_grad():
@@ -131,22 +139,18 @@ if __name__ == '__main__':
                     test_losses.append(loss.item())
 
                     test_acc.append(torch.sum(torch.argmax(F.softmax(c_t), dim=1) == y_t).item() / float(len(c_t)))
-            if epoch % 10 == 0:
-                for ii in range(4):
-                    fig = plt.figure
-                    plt.imshow(x_t[ii].detach().cpu().numpy(), cmap='gray')
-                    plt.show()
-                    plt.imshow(x_rec_t[ii].detach().cpu().numpy(), cmap='gray')
-                    plt.show()
+                val_losses.append(np.mean(test_losses))
+                val_acc.append(np.mean(test_acc))
+
             print('test_epoch {} test_loss {:.4f} test_acc {:.4f} '
-                              .format(epoch, np.mean(test_losses), np.mean(test_acc)))
-            print(y_t)
-            print(torch.argmax(F.softmax(c_t), dim=1))
+                  .format(epoch, np.mean(test_losses), np.mean(test_acc)))
+
             scheduler.step(np.mean(test_losses))
-            torch.save(model.state_dict(), 'mnist_data_model_weights_one_clip1_hs128_class.pth')
+            torch.save(model.state_dict(), 'mnist_data_model_weights_{}.pth'.format(suff))
+        plot_err_epoch(train_losses, val_losses, train_acc, val_acc, args.epochs, suff, parser.description)
 
     if do_test:
-        model.load_state_dict(torch.load('mnist_data_model_weights_one_clip1_hs128_class.pth'))
+        model.load_state_dict(torch.load('mnist_data_model_weights_{}.pth'.format(suff)))
         model.eval()
         with torch.no_grad():
             test_losses = []
@@ -176,5 +180,3 @@ if __name__ == '__main__':
 
             print('test_loss {:.4f} test_acc {:.4f} '
                   .format(np.mean(test_losses), np.mean(test_acc)))
-            print(y)
-            print(torch.argmax(F.softmax(c), dim=1))
